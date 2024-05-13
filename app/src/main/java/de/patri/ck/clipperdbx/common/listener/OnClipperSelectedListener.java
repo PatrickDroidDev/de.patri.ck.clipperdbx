997cf7bca873dcc856bbb5d361cbf46d5d9669f9
PatrickDroidDev <PatrickDroidDev@users.noreply.github.com>
2024-05-13 05:50:39 +0200

    Create OnClipperSelectedListener.java

---
+++ app/src/main/java/de/patri/ck/clipperdbx/common/listener/OnClipperSelectedListener.java
@@ -0,0 +1,7 @@
+package de.patri.ck.clipperdbx.common.listener;
+
+import de.patri.ck.clipperdbx.common.model.Clipper;
+
+public interface OnClipperSelectedListener {
+  void onClipperSelect(int _id);
+}
